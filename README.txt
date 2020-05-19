82a9f476416efe9403da25b84427907009142cd3
Agnieszka Kaminska <agnieszka.kaminsa@ksiegarnia.edu.pl>
2020-05-19 11:28:18 +0200

    animation scroll menu

---
+++ README.txt
@@ -0,0 +1,7 @@
+Challenge#2
+
+Pomysł na projekt zaczerpnięty z grupy FB - Weekly Webdev Challenge - #chellenge 2
+
+Strona została wykonana w technologi HTML + CSS + JS z elementami JQuery (animacja przewijanego menu).
+
+Kod CSS został napisany z użyciem SCSS.
--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-document.addEventListener('DOMContentLoaded', function (){
-    console.log("hej");
-   let head = document.querySelector(".head");
-   
-   function scroll(){
-   if(window.scrollY > 150){
-       head.classList.add("navbar-fixed-top")
-   }else
-   head.classList.remove("navbar-fixed-top")
-   }
+document.addEventListener('DOMContentLoaded', function () {
 
-   window.addEventListener("scroll", scroll);
+    //#region  przyklejenie menu  
+    let head = document.querySelector(".head");
+    function scroll() {
+        if (window.scrollY > 5) {
+            head.classList.add("navbar-fixed-top")
+        } else
+            head.classList.remove("navbar-fixed-top")
+    }
+
+    window.addEventListener("scroll", scroll);
 
 })
+//#endregion
+
+// #region JQuery animacja menu podczas przewijania
+function activeMenu() {
+    const scroll = $(window).scrollTop(); //ustalam pozycję scroola
+
+    //ustalam w którym miejscu zczyna się każdy article
+    const aboutUs = $("#about-us").offset().top;
+    const ourService = $("#our-service").offset().top;
+    const gallery = $("#gallery").offset().top;
+    const blog = $("#blog").offset().top;
+    const contact = $("#contact").offset().top;
+
+    // sprawdzam czy pozycja scroll osiągneła pozycje danego article i w zalezności dodaje lub usuwam klasę z aniamcją
+    if (scroll < aboutUs) {
+        $("nav li hr").not(".hr1").removeClass("li-hr");
+        $(".hr1").addClass("li-hr");
+    } else if (scroll < ourService) {
+        $("nav li hr").not(".hr2").removeClass("li-hr");
+        $(".hr2").addClass("li-hr");
+    } else if (scroll < gallery){
+        $("nav li hr").not(".hr3").removeClass("li-hr");
+        $(".hr3").addClass("li-hr");
+    } else if (scroll < blog){
+        $("nav li hr").not(".hr4").removeClass("li-hr");
+        $(".hr4").addClass("li-hr");
+    }  else if (scroll < blog){
+        $("nav li hr").not(".hr5").removeClass("li-hr");
+        $(".hr5").addClass("li-hr");
+    }  
+    else {
+        $("nav li hr").not(".hr6").removeClass("li-hr");
+        $(".hr6").addClass("li-hr");
+    }
+}
+// dodaje nasłuch na scroll podczas przewijania okna i wywołuję funkcję JQuery
+$(window).on("scroll", activeMenu);
+
+//#endregion
